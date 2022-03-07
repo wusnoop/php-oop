9907cc99fd08e028bad9666e40c0157b709323bd
Konstantin <wusnoop@gmail.com>
2022-03-07 11:03:51 +0300

    add SQL

--- news/NewsDB.class.php
+++ news/NewsDB.class.php
@@ -1,1 +1,23 @@
 <?php
+require_once "INewsDB.class.php";
+class NewsDB implements INewsDB{
+    const DB_NAME = "../news.db";
+    private $_db = null;
+    function __get($name)
+    {
+        if ($name == "db")
+            return $this->_db;
+        throw new Exception("Unknown property");
+    }
+    function __construct(){
+            $this->_db = new SQLite3(self::DB_NAME);
+    }
+    function __destruct(){
+            unset($this->_db);
+        }
+    function saveNews($title, $category, $description, $source){}
+    function getNews(){}
+    function deleteNews($id){}
+
+}
+$new = new NewsDB();
